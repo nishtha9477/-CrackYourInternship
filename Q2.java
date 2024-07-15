b279dc5ffecd82922c0bacc1c38827ddb4ad50db
Nishtha Maurya <108268302+nishtha9477@users.noreply.github.com>
2024-07-15 15:24:21 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,23 @@
+// Move Zeroes(Leetcode)
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int size=nums.length;
+      if(size==0|| size==1){
+        return;
+      }
+      int nz=0, z=0;
+      while(nz<size){
+        if(nums[nz]!=0){
+            int temp=nums[nz];
+            nums[nz]=nums[z];
+            nums[z]=temp;
+            nz++;
+            z++;
+        }
+        else{
+            nz++;
+        }
+      }
+    }
+}
